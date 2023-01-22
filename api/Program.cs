3918411c4e425de66c065aea19c716b005639b63
neosmiles <eselen.aig@gmail.com>
2023-01-22 18:02:37 +0100

    Authentication packages added

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,8 @@
+using System.Text;
+using api.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +11,40 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+// within this section we are configuring the authentication and setting the default scheme
+builder.Services.AddAuthentication(options =>
+     {
+       options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+       options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+       options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+     })
+
+// Adding Jwt Bearer
+.AddJwtBearer(jwt =>
+{
+  var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+
+  jwt.SaveToken = true;
+  jwt.TokenValidationParameters = new TokenValidationParameters
+  {
+    ValidateIssuerSigningKey = true, // this will validate the 3rd part of the jwt token using the secret that we added in the appsettings and verify we have generated the jwt token
+    IssuerSigningKey = new SymmetricSecurityKey(key),  // Add the secret key to our Jwt encryption
+    ValidateIssuer = false,
+    ValidateAudience = false,
+    RequireExpirationTime = false,
+    ValidateLifetime = true
+  };
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
